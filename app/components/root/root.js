380f517fecd3af74f2daf8e5d0798e2751bf9f7e
Alex Ilyaev <alexilyaev@gmail.com>
2017-04-09 03:16:07 +0300

    Added all react-eslint rules

--- app/components/root/root.js
+++ app/components/root/root.js
@@ -4,10 +4,14 @@ import React from 'react';
 
 export default class Root extends React.Component {
 
+  renderHeading() {
+    return <h1>Welcome to React ES6 Starter!</h1>;
+  }
+
   render() {
     return (
       <div>
-        <h1>Welcome to React ES6 Starter!</h1>
+        { this.renderHeading() }
       </div>
     );
   }
