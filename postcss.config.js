5bca04d92fd27ab3ae386325d92f83b9ec355996
Alex Ilyaev <alexilyaev@gmail.com>
2018-10-24 22:25:54 +0300

    Add cssnano to postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  plugins: [autoprefixer()]
+  plugins: isProduction ? [autoprefixer(), cssnano()] : []
 };
